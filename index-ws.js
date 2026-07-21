74ecb496359b4882c66eade811926bf7188a0726
Paulina <paulina@Paulinas-MacBook-Pro.local>
2026-07-21 15:02:09 +0200

    add db logging

--- index-ws.js
+++ index-ws.js
@@ -1,5 +1,4 @@
 const express = require('express');
-
 const server = require('http').createServer();
 const app = express();
 
@@ -15,17 +14,28 @@ server.listen(3000, function() {
 
 /** Begin WebSocket */
 const { Server : WebSocketServer, WebSocket} = require('ws');
+
 const wss = new WebSocketServer({ server: server });
 
+process.on('SIGINT', () => {
+    wss.clients.forEach(function each(client) {
+        client.close();
+    })
+    server.close(() => {
+        shutdownDB();
+    })
+})
 wss.on('connection', function connection(ws){
-    console.log('Client connected');
     const numClients = wss.clients.size;
-    console.log('Clients connected: ', numClients);
+    console.log('Client connected', numClients);
 
     wss.broadcast(`Current visitors: ${numClients}`);
     if(ws.readyState === WebSocket.OPEN){
         ws.send(`Welcome to the chat!`);
     }
+    db.run(`INSERT INTO visitors (count, time)
+        VALUES (${numClients}, datetime('now'))    
+    `);
     ws.on('close', function close() {
         wss.broadcast(`Current visitors: ${numClients - 1}`);
         console.log('Client disconnected');
@@ -35,4 +45,31 @@ wss.broadcast = function broadcast(data) {
     wss.clients.forEach(function each(client) {
         client.send(data);
     })
+}
+/** end Websockets */
+
+/** begin database */
+const sqlite = require('sqlite3');
+
+const db = new sqlite.Database(':memory:');
+
+db.serialize(() => {
+    db.run(`
+        CREATE TABLE visitors (
+            count INTEGER,
+            time TEXT
+        )    
+    `)
+})
+
+function getCounts() {
+    db.each('SELECT * FROM visitors', (err, row) => {
+        console.log('row', row);
+    })
+}
+
+function shutdownDB() {
+    console.log('Shutting down db');
+    getCounts();
+    db.close();
 }
